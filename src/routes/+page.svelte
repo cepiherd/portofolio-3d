2247f0eb580f462fd55d7c3695295b5f7a824a9e
ngab_cepi <cepiherd@gmail.com>
2023-01-31 09:04:35 +0700

    :hammer: adding cnt logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,45 +4,73 @@
     import { degToRad } from "three/src/math/MathUtils";
     import { GLTF } from "@threlte/extras";
     import { AmbientLight } from "@threlte/core";
-    import { fade} from 'svelte/transition'
+    import { fade } from "svelte/transition";
     const scale = spring(1);
-    let deg = 80
+    let deg = 80;
     let x;
 </script>
 
 <div class="min-h-[3000px] px-10 text-white">
     <div class="fixed left-0 bg-yellow-500  ">
-        {#if  x <100 }
-        <h1 class="absolute bottom-0 mx-auto font-extrabold text-[170px] translate-x-10 z-50" transition:fade>Scroll Down.</h1>
+        {#if x < 100}
+            <h1
+                class="absolute bottom-0 mx-auto font-extrabold text-[170px] translate-x-10 z-50"
+                transition:fade
+            >
+                Scroll Down.
+            </h1>
         {/if}
-        {#if x >200 && x <400 }
-            <h1 class="absolute font-extrabold text-[170px] translate-x-10 z-50" transition:fade>Hello.</h1>
+        {#if x > 200 && x < 400}
+            <h1
+                class="absolute font-extrabold text-[170px] translate-x-10 z-50"
+                transition:fade
+            >
+                Hello.
+            </h1>
         {/if}
-        {#if x >400 }
-            <h1 class=" absolute bottom-10 font-extrabold text-[150px] translate-x-10 z-50" transition:fade>Im Cepi.</h1>
-            
+        {#if x > 400}
+            <h1
+                class=" absolute bottom-10 font-extrabold text-[150px] translate-x-10 z-50"
+                transition:fade
+            >
+                Im Cepi.
+            </h1>
         {/if}
-        {#if x >600 }
-        <h1 class="absolute bottom-0  text-3xl font-extrabold"  transition:fade style="transform: translate(960px, -500px);">Frontend Developer</h1>
-        <p class="absolute bottom-0  text-lg font-extrabold" transition:fade style="transform: translate(960px, -480px);">at cnt.id</p>
-        {/if}
-
-
-       
+        {#if x > 600}{/if}
     </div>
 
     <div class="fixed left-0 mx-auto top-0 bottom-0 w-full ">
-        {#if x >700 }
-    <button transition:fade class="absolute bottom-0   border border-white p-3 z-50" style="transform: translate(960px, -420px);">
-        <a href="https://www.linkedin.com/in/cepi-herdiansyah/"> Hire me !</a>
-       </button>
-{/if}
+        {#if x > 700}
+            <h1
+                class="absolute bottom-0  text-3xl font-extrabold"
+                transition:fade
+                style="transform: translate(960px, -500px);"
+            >
+                Frontend Developer
+            </h1>
+            <p
+                class="absolute bottom-0  text-lg font-extrabold"
+                transition:fade
+                style="transform: translate(960px, -480px);"
+            >
+                at cnt.id
+            </p>
+            <button
+                transition:fade
+                class="absolute bottom-0   border border-white p-3 z-50"
+                style="transform: translate(960px, -420px);"
+            >
+                <a href="https://www.linkedin.com/in/cepi-herdiansyah/">
+                    Hire me !</a
+                >
+            </button>
+        {/if}
         <Canvas>
-            <T.PerspectiveCamera makeDefault position={[40 , 90, 50]} fov={20}>
+            <T.PerspectiveCamera makeDefault position={[40, 90, 50]} fov={20}>
                 <OrbitControls
                     maxPolarAngle={degToRad(40)}
                     enableZoom={false}
-                    target={{ y: 0 , z:1 }}
+                    target={{ y: 0, z: 1 }}
                 />
             </T.PerspectiveCamera>
 
@@ -72,7 +100,7 @@
                         receiveShadow
                         url={"./tree.glb"}
                         interactive
-                        scale={ 0.4 + x/1000}
+                        scale={0.4 + x / 1000}
                         position={{ y: 0.1 }}
                     />
                     <GLTF
@@ -80,9 +108,9 @@
                         receiveShadow
                         url={"./cybertruck.glb"}
                         interactive
-                        scale={ 0.4 + x/1000}
-                        position={{ x: 1 + x/30  }}
-                        rotation={x/100}
+                        scale={0.4 + x / 1000}
+                        position={{ x: 1 + x / 30 }}
+                        rotation={x / 100}
                     />
 
                     <GLTF
@@ -90,8 +118,17 @@
                         receiveShadow={true}
                         url={"./cybertruck.glb"}
                         interactive
-                        scale={ 0.4 + x/1000}
-                        position={{ x: -70 + x/30 , z:8  }}
+                        scale={0.4 + x / 1000}
+                        position={{ x: -70 + x / 30, z: 8 }}
+                    />
+
+                    <GLTF
+                        castShadow
+                        receiveShadow={true}
+                        url={"./cnt.glb"}
+                        interactive
+                        scale={0.4 + x / 1000}
+                        position={{ y: 0, z: -4 - x / 100 , x:-2 + x/10000 }}
                     />
                     <!-- <T.BoxGeometry  /> -->
                     <T.MeshStandardMaterial color="#ffffff" />
@@ -107,8 +144,7 @@
     </div>
 </div>
 
-
-<svelte:window bind:scrollY={x}/>
+<svelte:window bind:scrollY={x} />
 
 <style>
     div {
